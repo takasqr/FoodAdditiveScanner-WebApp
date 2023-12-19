7e6a2ee42db1b7bf3f2d4a1ba4162e127ce03954
takasqr <95556025+takasqr@users.noreply.github.com>
2023-12-19 21:38:16 +0900

    tailwindcss を導入

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,10 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
