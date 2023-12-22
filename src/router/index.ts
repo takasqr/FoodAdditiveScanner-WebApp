66c30a5cc9fe31c2a46027c236490524d23dc605
takasqr <95556025+takasqr@users.noreply.github.com>
2023-12-22 23:10:57 +0900

    変更

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,6 +16,22 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/signin',
+      name: 'signin',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/page/SigninPage.vue')
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/page/SignupPage.vue')
     }
   ]
 })
