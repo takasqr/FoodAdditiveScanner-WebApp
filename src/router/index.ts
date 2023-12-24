856a04fa8507ef0cf233877d474428de70976183
takasqr <95556025+takasqr@users.noreply.github.com>
2023-12-24 14:49:03 +0900

    Scanner

--- src/router/index.ts
+++ src/router/index.ts
@@ -32,6 +32,22 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/page/SignupPage.vue')
+    },
+    {
+      path: '/scanner',
+      name: 'scanner',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/page/ScannerPage.vue')
+    },
+    {
+      path: '/scanner',
+      name: 'scanner',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/page/ScannerPage.vue')
     }
   ]
 })
