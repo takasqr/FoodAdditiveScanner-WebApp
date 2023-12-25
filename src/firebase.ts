b6c937283758cde7c66ae83818304ca5018e5fdd
takasqr <95556025+takasqr@users.noreply.github.com>
2023-12-25 22:59:18 +0900

    firebase

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+
+console.log(import.meta.env.VITE_FB_API_KEY)
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FB_API_KEY,
+  authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FB_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FB_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FB_APP_ID
+};
+
+const app = initializeApp(firebaseConfig);
