93c66ee3eb90b1bc77beb60d55a4b7892196cc73
takasqr <95556025+takasqr@users.noreply.github.com>
2024-01-10 21:26:21 +0900

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({ registerType: 'autoUpdate' })
   ],
   resolve: {
     alias: {
