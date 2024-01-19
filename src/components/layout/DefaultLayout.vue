d98371e8aa72239f52e9d3442b281a9638329f6b
takasqr <95556025+takasqr@users.noreply.github.com>
2024-01-19 21:51:08 +0900

    Update DefaultLayout.vue

--- src/components/layout/DefaultLayout.vue
+++ src/components/layout/DefaultLayout.vue
@@ -3,28 +3,28 @@
 
     <Banner></Banner>
 
-    <div class="mt-8 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <div class="mt-4 lg:mt-8 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
 
       <!-- We've used 3xl here, but feel free to try other max-widths based on your needs -->
       <div class="mx-auto max-w-3xl">
 
         <Breadcrumb></Breadcrumb>
 
-        <div class="mt-4">
+        <div class="mt-4 lg:mt-10">
           <!-- Content goes here -->
           <slot />
         </div>
       </div>
     </div>
 
-    <Footer></Footer>
+    <FooterSimple></FooterSimple>
 
   </SideBarMenu>
 </template>
 
 <script setup lang="ts">
 import SideBarMenu from '@/components/basic/SideBarMenu.vue'
-import Footer from '@/components/basic/Footer.vue'
+import FooterSimple from '@/components/basic/FooterSimple.vue'
 import Breadcrumb from '@/components/basic/Breadcrumb.vue'
 import Banner from '@/components/basic/Banner.vue'
 </script>
