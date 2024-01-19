bdcacbb3b5b8713466dc7d121af438d56a0beb44
takasqr <95556025+takasqr@users.noreply.github.com>
2024-01-19 21:52:03 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,59 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: {
+          light: '#e06666',
+          DEFAULT: '#e06666',
+          dark: '#f1c232'
+        },
+        primary: {
+          light: '#6366f1',
+          DEFAULT: '#6366f1',
+          dark: '#0e7490',
+        },
+        primaryVia: {
+          light: '#29070d',
+          DEFAULT: '#a855f7',
+          dark: '#0e7490',
+        },
+        primaryTo: {
+          light: '#29070d',
+          DEFAULT: '#ec4899',
+          dark: '#0e7490',
+        },
+        secondary: {
+          light: '#6366f1',
+          DEFAULT: '#6366f1',
+          dark: '#0e7490',
+        },
+        secondaryVia: {
+          light: '#29070d',
+          DEFAULT: '#a855f7',
+          dark: '#0e7490',
+        },
+        secondaryTo: {
+          light: '#29070d',
+          DEFAULT: '#ec4899',
+          dark: '#0e7490',
+        },
+        error: '#B00020',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FB8C00',
+        background: {
+          light: '#29070d',
+          DEFAULT: '#ec4899',
+          dark: '#0e7490',
+        },
+        surface: {
+          light: '#29070d',
+          DEFAULT: '#ec4899',
+          dark: '#0e7490',
+        },
+      },
+    },
   },
   plugins: [],
 }
