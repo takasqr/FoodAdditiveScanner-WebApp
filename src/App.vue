841b69cb15737e025940daf5a461e1807281db1b
takasqr <95556025+takasqr@users.noreply.github.com>
2024-01-23 22:44:12 +0900

    GTM

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,25 @@ import Notification from '@/components/basic/Notification.vue'
 import Dialog from '@/components/basic/Dialog.vue'
 import { useThemeStore } from '@/stores/theme'
 import { useBannerStore } from '@/stores/banner';
+import { useHead } from '@unhead/vue'
+
+useHead({
+  script: [
+    {
+      innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','${import.meta.env.VITE_GTM_ID}');` 
+    }
+  ],
+  noscript: [
+    {
+      innerHTML: `<iframe src="https://www.googletagmanager.com/ns.html?id=${import.meta.env.VITE_GTM_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+      tagPosition: 'bodyOpen'
+    }
+  ]
+})
 
 // ダークモード設定
 const themeStore = useThemeStore()
