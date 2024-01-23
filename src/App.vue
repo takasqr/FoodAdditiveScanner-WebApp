f12da6255e08e2e6c3a8de1a73fc99e7fe4addb4
takasqr <95556025+takasqr@users.noreply.github.com>
2024-01-23 22:35:21 +0900

    コード、ファイル削除

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import AccountService from './model/AccountService';
 import router from './router';
 import Notification from '@/components/basic/Notification.vue'
 import Dialog from '@/components/basic/Dialog.vue'
@@ -20,23 +17,6 @@ bannerStore.open({ title: 'セール', contentText: '初回５０パーセント
 </script>
 
 <template>
-  <!-- <header>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header> -->
-
   <div class="bg-white dark:bg-slate-800 min-h-screen">
     <RouterView />
 
