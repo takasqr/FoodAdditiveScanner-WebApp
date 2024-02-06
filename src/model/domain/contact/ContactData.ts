ee8a291ce744553acec6e177f61c95100d7f738f
takasqr <95556025+takasqr@users.noreply.github.com>
2024-02-06 21:30:52 +0900

    DTO を interface から class に変更

--- src/model/domain/contact/ContactData.ts
+++ src/model/domain/contact/ContactData.ts
@@ -1,5 +1,15 @@
-interface ContactData extends DTO {
+import type { Contact } from "./Contact";
+
+class ContactData implements DTO {
   id: string
   uid: string;
   content: string;
+
+  constructor(contact: Contact) {
+    this.id = contact.id
+    this.uid = contact.uid
+    this.content = contact.content
+  }
 }
+
+export { ContactData }
