6096221088a502e2f5eb44670f179f6d92b2ab6f
takasqr <95556025+takasqr@users.noreply.github.com>
2024-02-09 22:28:08 +0900

    DatePicker.vue

--- src/ui/basic/date-picker/DatePicker.vue
+++ src/ui/basic/date-picker/DatePicker.vue
@@ -1,24 +1,22 @@
 <template>
   <div class="mt-10 text-center">
     <div class="flex items-center text-gray-900">
-      <button type="button" class="-m-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500">
+      <button @click="previousMonth" type="button" class="-m-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500">
         <span class="sr-only">Previous month</span>
         <ChevronLeftIcon class="h-5 w-5" aria-hidden="true" />
       </button>
-      <div class="flex-auto text-sm font-semibold">{{ formattedMonth }}</div>
-      <button type="button" class="-m-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500">
+      <div class="flex-auto text-sm font-semibold">{{ year + '年 ' + month + '月' }}</div>
+      <button @click="nextMonth" type="button" class="-m-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500">
         <span class="sr-only">Next month</span>
         <ChevronRightIcon class="h-5 w-5" aria-hidden="true" />
       </button>
     </div>
+    
+    <!-- Week -->
     <div class="mt-6 grid grid-cols-7 text-xs leading-6 text-gray-500">
-      <div>M</div>
-      <div>T</div>
-      <div>W</div>
-      <div>T</div>
-      <div>F</div>
-      <div>S</div>
-      <div>S</div>
+      <template v-for="dayOfTheWeek in daysOfTheWeek" key="dayOfTheWeek">
+        <div>{{ dayOfTheWeek }}</div>
+      </template>
     </div>
     <div class="isolate mt-2 grid grid-cols-7 gap-px rounded-lg bg-gray-200 text-sm shadow ring-1 ring-gray-200">
       <button v-for="(day, dayIdx) in days" :key="day.date" @click="changeDatePicker(day)" type="button" :class="['py-1.5 hover:bg-gray-100 focus:z-10', day.isCurrentMonth ? 'bg-white' : 'bg-gray-50', (day.isSelected || day.isToday) && 'font-semibold', day.isSelected && 'text-white', !day.isSelected && day.isCurrentMonth && !day.isToday && 'text-gray-900', !day.isSelected && !day.isCurrentMonth && !day.isToday && 'text-gray-400', day.isToday && !day.isSelected && 'text-indigo-600', dayIdx === 0 && 'rounded-tl-lg', dayIdx === 6 && 'rounded-tr-lg', dayIdx === days.length - 7 && 'rounded-bl-lg', dayIdx === days.length - 1 && 'rounded-br-lg']">
@@ -37,15 +35,16 @@
 <script setup lang="ts">
 import { ref, defineProps, defineEmits } from 'vue'
 import {
-  CalendarIcon,
   ChevronLeftIcon,
   ChevronRightIcon,
-  EllipsisHorizontalIcon,
-  MapPinIcon,
 } from '@heroicons/vue/20/solid'
-import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
-import { CalendarService } from '@/model/util/CalendarService'
-import { setEnvironmentData } from 'worker_threads';
+import dayjs from 'dayjs';
+import weekday from 'dayjs/plugin/weekday';
+import localeData from 'dayjs/plugin/localeData';
+import 'dayjs/locale/ja'; // 日本語ロケールをインポート
+
+dayjs.extend(weekday);
+dayjs.extend(localeData);
 
 // 'locale' プロパティの型を定義
 // 初期値付きの props
@@ -60,12 +59,24 @@ const props = withDefaults(defineProps<Props>(), {
 
 const value = ref('')
 const input = ref<HTMLElement | null>(null)
-
+const daysOfTheWeek = ref<String[]>([])
 
 // emit を定義
 const emit = defineEmits<{
   change: [value: Event]
 }>()
+
+// 曜日を設定する
+daysOfTheWeek.value = getDaysOfTheWeek(props.locale)
+
+// ロケールを指定すると曜日を返す関数
+function getDaysOfTheWeek(locale: string): string[] {
+  if (locale === 'ja') {
+    return ['月', '火', '水', '木', '金', '土', '日']
+  } else {
+    return ['M', 'T', 'W', 'T', 'F', 'S', 'S']
+  }
+}
 
 function changeDatePicker(day: Day) {
 
@@ -82,18 +93,21 @@ function changeDatePicker(day: Day) {
 }
 
 function changeInput(event: Event) {
+
+  console.log('input chenge.')
+
+  // days に反映
+  const target = event.target as HTMLInputElement
+  selectDay(target.value)
+
   emit('change', event)
 }
-
-const formattedMonth = ref('')
-
-const calendarService = new CalendarService(props.locale)
 
 setInitialData()
 
 // 初期データをセットする
 function setInitialData() {
-  formattedMonth.value = calendarService.getLocaleFormattedMonth()
+
 }
 
 interface Day {
@@ -103,54 +117,84 @@ interface Day {
   isSelected?: boolean;
 }
 
-const days: Day[] = [
-  { date: '2021-12-27' },
-  { date: '2021-12-28' },
-  { date: '2021-12-29' },
-  { date: '2021-12-30' },
-  { date: '2021-12-31' },
-  { date: '2022-01-01', isCurrentMonth: true },
-  { date: '2022-01-02', isCurrentMonth: true },
-  { date: '2022-01-03', isCurrentMonth: true },
-  { date: '2022-01-04', isCurrentMonth: true },
-  { date: '2022-01-05', isCurrentMonth: true },
-  { date: '2022-01-06', isCurrentMonth: true },
-  { date: '2022-01-07', isCurrentMonth: true },
-  { date: '2022-01-08', isCurrentMonth: true },
-  { date: '2022-01-09', isCurrentMonth: true },
-  { date: '2022-01-10', isCurrentMonth: true },
-  { date: '2022-01-11', isCurrentMonth: true },
-  { date: '2022-01-12', isCurrentMonth: true, isToday: true },
-  { date: '2022-01-13', isCurrentMonth: true },
-  { date: '2022-01-14', isCurrentMonth: true },
-  { date: '2022-01-15', isCurrentMonth: true },
-  { date: '2022-01-16', isCurrentMonth: true },
-  { date: '2022-01-17', isCurrentMonth: true },
-  { date: '2022-01-18', isCurrentMonth: true },
-  { date: '2022-01-19', isCurrentMonth: true },
-  { date: '2022-01-20', isCurrentMonth: true },
-  { date: '2022-01-21', isCurrentMonth: true },
-  { date: '2022-01-22', isCurrentMonth: true, isSelected: true },
-  { date: '2022-01-23', isCurrentMonth: true },
-  { date: '2022-01-24', isCurrentMonth: true },
-  { date: '2022-01-25', isCurrentMonth: true },
-  { date: '2022-01-26', isCurrentMonth: true },
-  { date: '2022-01-27', isCurrentMonth: true },
-  { date: '2022-01-28', isCurrentMonth: true },
-  { date: '2022-01-29', isCurrentMonth: true },
-  { date: '2022-01-30', isCurrentMonth: true },
-  { date: '2022-01-31', isCurrentMonth: true },
-  { date: '2022-02-01' },
-  { date: '2022-02-02' },
-  { date: '2022-02-03' },
-  { date: '2022-02-04' },
-  { date: '2022-02-05' },
-  { date: '2022-02-06' },
-]
+const year = ref<number>(new Date().getFullYear())
+const month = ref<number>(new Date().getMonth() + 1)
+const days = ref<Day[]>([])
+
+// 日付データを生成
+days.value = generateCalendarDays({ year: year.value, month: month.value, locale: props.locale })
 
 function getDayDisplayString(day: Day): string {
 
     const date: string = day.date
     return date.split('-').pop()?.replace(/^0/, '') || ''
+}
+
+function generateCalendarDays({ year, month, locale, selectedDates }: { year: number, month: number, locale: string, selectedDates?: string[] }): Day[] {
+  dayjs.locale(locale); // ロケールを設定
+  const days: Day[] = [];
+  const today = dayjs().startOf('day').format('YYYY-MM-DD');
+
+  // new Date に month を流す時は -1 する必要があるので注意
+  const monthIndex = month - 1
+
+  // 月の最初の日と最後の日を取得
+  const firstDayOfMonth = dayjs(new Date(year, monthIndex, 1));
+  const lastDayOfMonth = dayjs(new Date(year, monthIndex + 1, 0));
+
+  // 前月の最後の月曜日から始まるように調整
+  let startDay = firstDayOfMonth.subtract((firstDayOfMonth.weekday() + 6) % 7, 'day');
+
+  // 次月の最初の日曜日で終わるように調整
+  let endDay = lastDayOfMonth.add(7 - lastDayOfMonth.weekday(), 'day');
+
+  for (let d = dayjs(startDay); d.isBefore(endDay) || d.isSame(endDay, 'day'); d = d.add(1, 'day')) {
+    days.push({
+      date: d.format('YYYY-MM-DD'),
+      isCurrentMonth: d.month() === monthIndex,
+      isToday: d.format('YYYY-MM-DD') === today,
+      isSelected: selectedDates ? selectedDates.includes(d.format('YYYY-MM-DD')) : false,
+    });
+  }
+
+  return days;
+}
+
+function nextMonth() {
+
+  if (month.value < 12) {
+    month.value++
+  } else {
+    month.value = 1
+    year.value++
+  }
+
+  // 日付データを生成
+  days.value = generateCalendarDays({ year: year.value, month: month.value, locale: props.locale })
+}
+
+function previousMonth() {
+  
+  if (month.value > 1) {
+    month.value--
+  } else {
+    month.value = 12
+    year.value--
+  }
+
+  // 日付データを生成
+  days.value = generateCalendarDays({ year: year.value, month: month.value, locale: props.locale })
+}
+
+function selectDay(selectedDate: string) {
+
+  console.log(selectedDate)
+
+  const newDays =  days.value.map(day => ({
+    ...day,
+    isSelected: day.date === selectedDate ? true : false
+  }));
+
+  days.value = newDays
 }
 </script>
