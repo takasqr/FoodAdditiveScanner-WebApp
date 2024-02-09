382724e06e518c6aa9b99d1ec77aa1a325e1be0e
takasqr <95556025+takasqr@users.noreply.github.com>
2024-02-09 22:29:38 +0900

    ArticleListItem.vue

--- src/ui/template/article-list-item/ArticleListItem.vue
+++ src/ui/template/article-list-item/ArticleListItem.vue
@@ -2,14 +2,16 @@
   <!-- <div class="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3"> -->
     <article class="flex flex-col items-start justify-between">
       <div class="relative w-full">
-        <img :src="props.article.cover" alt="" class="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]" />
-        <div class="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+        <a :href="props.article._path">
+          <img :src="props.article.cover" alt="" class="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]" />
+          <div class="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+        </a>
       </div>
       <div class="max-w-xl">
         <div class="mt-1 flex items-center gap-x-4 text-xs">
           <time :datetime="props.article.createDate" class="text-gray-500">{{ props.article.createDate }}</time>
-          <!-- <a :href="props.article.category.href" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">{{ props.article.category.title }}</a> -->
-          <p class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">{{ props.article.category }}</p>
+          <a :href="'/' + props.article.category" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">{{ props.article.category }}</a>
+          <!-- <p class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">{{ props.article.category }}</p> -->
         </div>
         <div class="group relative">
           <h3 class="mt-0 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
