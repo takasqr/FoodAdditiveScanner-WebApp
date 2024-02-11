7de9dd18edb459952541973d97ee4e26ac5b6514
takasqr <95556025+takasqr@users.noreply.github.com>
2024-02-11 23:45:52 +0900

    Card、border-solid を指定

--- src/ui/basic/card/Card.vue
+++ src/ui/basic/card/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white border border-gray-300 rounded-lg">
+  <div class="bg-white border border-solid border-gray-300 rounded-lg">
     <!-- <div class="px-4 py-4"> -->
       <slot />
     <!-- </div> -->
