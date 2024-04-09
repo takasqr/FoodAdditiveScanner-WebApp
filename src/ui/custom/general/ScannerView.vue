5cfcb49d4a4e62742d11fd2c1767db78cba68837
takasqr <95556025+takasqr@users.noreply.github.com>
2024-04-10 05:43:38 +0900

    Update ScannerView.vue

--- src/ui/custom/general/ScannerView.vue
+++ src/ui/custom/general/ScannerView.vue
@@ -4,4 +4,9 @@
 
 <script setup lang="ts">
 import ImageUpload from '@/ui/template/image-upload/ImageUpload.vue';
+import { FoodAdditiveApplicationService } from '@/model/domain/food-additive/FoodAdditiveApplicationService';
+import { FoodAdditiveRepository } from '@/model/domain/food-additive/FoodAdditiveRepository';
+
+const foodAdditiveRepository = new FoodAdditiveRepository
+const foodAdditiveApplicationService = new FoodAdditiveApplicationService(foodAdditiveRepository)
 </script>
