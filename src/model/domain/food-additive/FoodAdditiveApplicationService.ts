6e24c3f3e3f1106d9e38a97e9a3901995ce68fb3
takasqr <95556025+takasqr@users.noreply.github.com>
2024-04-10 05:44:17 +0900

    Update FoodAdditiveApplicationService.ts

--- src/model/domain/food-additive/FoodAdditiveApplicationService.ts
+++ src/model/domain/food-additive/FoodAdditiveApplicationService.ts
@@ -1,9 +1,25 @@
+import type { FoodAdditive } from "./FoodAdditive"
+import type { IFoodAdditiveRepository } from "./IFoodAdditiveRepository"
+
 class FoodAdditiveApplicationService implements ApplicationService {
 
   private foodAdditiveRepository: IFoodAdditiveRepository
+
+  private foodAdditives: FoodAdditive[] 
   
-  constructor(foodAdditiveRepository: IContactRepository) {
+  constructor(foodAdditiveRepository: IFoodAdditiveRepository) {
     this.foodAdditiveRepository = foodAdditiveRepository
+
+    this.foodAdditives = []
+
+    // 添加物リストを初期化
+    this.initioalizeFoodAdditivesList()
+  }
+
+  // 初期化する
+  initioalizeFoodAdditivesList() {
+
+    this.foodAdditiveRepository.getAll()
   }
 }
 
